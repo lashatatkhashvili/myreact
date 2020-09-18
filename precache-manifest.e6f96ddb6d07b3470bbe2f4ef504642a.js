f297386414ad959413ea7091b3ce72e6e0c6fc07
lashatatkhashvili <59742532+lashatatkhashvili@users.noreply.github.com>
2020-09-18 19:14:07 +0400

    Updates

---
+++ precache-manifest.e6f96ddb6d07b3470bbe2f4ef504642a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86143909a04a7b560f8b7d4e764df733",
+    "url": "/lashatatkhashvili/myreact.git/index.html"
+  },
+  {
+    "revision": "4658b24fe96bb7577299",
+    "url": "/lashatatkhashvili/myreact.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "472fc2beade2db0707a9",
+    "url": "/lashatatkhashvili/myreact.git/static/js/2.f1ca1655.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lashatatkhashvili/myreact.git/static/js/2.f1ca1655.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4658b24fe96bb7577299",
+    "url": "/lashatatkhashvili/myreact.git/static/js/main.28e50ede.chunk.js"
+  },
+  {
+    "revision": "6edbd017aeb94321922c",
+    "url": "/lashatatkhashvili/myreact.git/static/js/runtime-main.827f4d53.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lashatatkhashvili/myreact.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
